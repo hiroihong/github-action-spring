b30e690c35f7aef292680950dd58ed1fadcc694a
hiroihong@gmail.com <hiroihong@gmail.com>
2025-04-17 21:36:30 +0900

    first

---
+++ github-action-server/src/main/java/com/example/github_action_server/GithubActionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_action_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionServerApplication.class, args);
+	}
+
+}
---
+++ github-action-server/src/test/java/com/example/github_action_server/GithubActionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_action_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
